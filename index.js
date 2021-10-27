76108c316c4089823b6b5218940092cf6730843c
devmarcos <devmarcosav@gmail.com>
2021-10-27 17:21:13 -0300

    fix:Refatorando

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 import {Cliente} from "./Cliente.js";
-import { Conta } from "./Conta.js";
-import {ContaCorrente} from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import {Gerente} from "./Funcionario/Gerente.js"
+import {Diretor} from "./Funcionario/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const diretor =  new Diretor("Rodrigo", 10000, 12345678900);
+diretor.cadastrarSenha("123456")
+const gerente =  new Gerente("Ricardo",  5000, 12378945601);
+gerente.cadastrarSenha("123");
 
-const contaCorrenteRicardo = new ContaCorrente(0, cliente1, 1001);
-contaCorrenteRicardo.depositar(500);
-contaCorrenteRicardo.sacar(100);
+const cliente = new Cliente("Lais", 78945612379, "456");
+const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
+const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 
-const cliente2 = new Cliente("Marcos", 999999909);
-const contaMarcos = new ContaPoupanca(0, cliente2, 1001);
-contaMarcos.sacar(50);
 
-const contaX = new Conta(0, cliente2, 1001);
+const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-console.log(contaX);
-
-console.log(contaCorrenteRicardo);
-console.log(contaMarcos);
+console.log(gerenteEstaLogado, diretorEstaLogado, clienteEstaLogado);
